41f0fb45d9e11d8c1e2e32331a027071a009639a
Aleisha <nelson.aleisha@gmail.com>
2016-02-29 12:39:13 -0800

    Added files via upload
    Included are the html and js files that are linked so that the quiz may access the topics array - which in this case are the world monuments.

---
+++ Basic-Quiz.js
@@ -0,0 +1,45 @@
+
+//Array of Monuments - contains images for all 4 topics, as well as questions asked and correct answers.
+var Monuments = [
+//Colosseum Topic
+	{name: "Colosseum",
+		image: "images/Colosseum.jpg",
+		questions: [{quest:"In which famous Italian city is this building located?",answer:"rome"},
+					{quest:"In what year was it built?",answer:"70-80 ad"},
+					{quest:"It is the largest ampitheatre ever built. True or False?",answer:"true"},
+					{quest:"It was used for Gladiator contests. True or False?",answer:"true"}
+				   ]
+	},   
+
+//Eiffel Tower Topic
+	{name: "Eiffel Tower",
+		image: "images/EiffelTower.jpg", 
+		questions: [{quest:"The Eiffel Tower is almost 1000 ft. tall. True or False?",answer:"true"},
+					{quest:"In which French city is it located?",answer:"paris"},
+					{quest:"In what year was the construction of it completed?",answer:"1889"},
+					{quest:"There is an elevator that can take you to the top. True or False?",answer:"true"}
+					]
+	},   
+
+//Statue of Liberty Topic
+	{name: "Statue of Liberty",
+		image: "images/StatueOfLiberty.jpg", 
+		questions: [{quest:"In which US state is this monument located?", answer:"new york"},
+					{quest:"In which US city is it located?", answer:"new york city"},
+					{quest:"It was built 20 years ago. True or False?", answer:"false"},  //it was built 84 years ago
+					{quest:"The Statue of Liberty is only 50 feet shorter than a football field. True or False?", answer:"true"}
+					]
+	},    
+
+//Golden Gate Bridge Topic
+	 {name: "Golden Gate Bridge",
+	 	image: "images/GoldenGate.jpg", 
+		questions: [{quest:"In which US state is this bridge located?",answer:"california"},
+					{quest:"In which US city is this bridge located?",answer:"san francisco"},
+					{quest:"This is the most photographed bridge in the world. True or False?",answer:"true"},
+					{quest:"Is the Golden Gate Bridge a suspension bridge?",answer:"yes"}
+					]
+
+	}   
+];
+
